15ab03220f18054fd1d28f6a40cd21177df17c6d
raffy-kebula <raffaele.aurucci@kebula.it>
2026-04-02 21:33:35 +0200

    update logic training cards on db

--- backend/database_models.py
+++ backend/database_models.py
@@ -3,11 +3,14 @@ from sqlalchemy import (
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy import UniqueConstraint, Enum
 
 Base = declarative_base()
 
 
+# -------------------------------------------------------------------------
 # USERS & PAYMENT
+# -------------------------------------------------------------------------
 class User(Base):
     __tablename__ = "users"
 
@@ -38,7 +41,7 @@ class CreditCard(Base):
     __tablename__ = "credit_cards"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     token = Column(String(128))
     last_4 = Column(String(4))
     brand = Column(String(50))
@@ -49,7 +52,10 @@ class CreditCard(Base):
     courses = relationship("CourseUserCard", back_populates="card")
 
 
+
+# -------------------------------------------------------------------------
 # SUBSCRIPTIONS & COURSES
+# -------------------------------------------------------------------------
 class Subscription(Base):
     __tablename__ = "subscriptions"
 
@@ -60,16 +66,18 @@ class Subscription(Base):
     description = Column(String(255))
 
     # relationship
-    users_cards = relationship("SubscriptionUserCard", back_populates="subscription", cascade="all, delete-orphan")
+    users_cards = relationship("SubscriptionUserCard", back_populates="subscription",
+                               cascade="all, delete-orphan")
 
 
 class SubscriptionUserCard(Base):
     __tablename__ = "subscription_user_cards"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     card_id = Column(Integer, ForeignKey("credit_cards.id"), nullable=False)
-    subscription_id = Column(Integer, ForeignKey("subscriptions.id"), nullable=False)
+    # IF subscription is deleted reimbursement management
+    subscription_id = Column(Integer, ForeignKey("subscriptions.id", ondelete="CASCADE"), nullable=False)
     init_date = Column(Date, nullable=False)
     expiry_date = Column(Date, nullable=False)
     automatic_renewal = Column(Boolean, default=False)
@@ -100,9 +108,10 @@ class CourseUserCard(Base):
     __tablename__ = "course_user_cards"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     card_id = Column(Integer, ForeignKey("credit_cards.id"), nullable=False)
-    course_id = Column(Integer, ForeignKey("courses.id"), nullable=False)
+    # IF course is deleted reimbursement management
+    course_id = Column(Integer, ForeignKey("courses.id", ondelete="CASCADE"), nullable=False)
     init_date = Column(Date, nullable=False)
     expiry_date = Column(Date, nullable=False)
     automatic_renewal = Column(Boolean, default=False)
@@ -117,8 +126,8 @@ class ReservationCourse(Base):
     __tablename__ = "reservation_courses"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    course_id = Column(Integer, ForeignKey("courses.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    course_id = Column(Integer, ForeignKey("courses.id", ondelete="CASCADE"), nullable=False)
     date = Column(Date, nullable=False)
     from_hour = Column(Time, nullable=False)
     to_hour = Column(Time, nullable=False)
@@ -128,48 +137,49 @@ class ReservationCourse(Base):
     course = relationship("Course", back_populates="reservations")
 
 
+
+# -------------------------------------------------------------------------
+# TRAINING CARDS
+# -------------------------------------------------------------------------
+import enum
+
+class WeekDay(enum.Enum):
+    Monday = "Monday"
+    Tuesday = "Tuesday"
+    Wednesday = "Wednesday"
+    Thursday = "Thursday"
+    Friday = "Friday"
+    Saturday = "Saturday"
+
 # TOPOLOGY OF EXERCISES
 class Exercise(Base):
     __tablename__ = "exercises"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), nullable=False)
+    name = Column(String(100), nullable=False, unique=True)
     muscle_group = Column(String(100), nullable=False)
     description = Column(String(255))
 
-    # relationship
-    training_exercises = relationship("TrainingExercise", back_populates="exercise", cascade="all, delete-orphan")
+    # relationships
+    card_exercises = relationship("TrainingCardExercise", back_populates="exercise")
 
 
-# GENERIC TRAINING EXERCISE
-class TrainingExercise(Base):
-    __tablename__ = "training_exercises"
-
-    id = Column(Integer, primary_key=True, index=True)
-    exercise_id = Column(Integer, ForeignKey("exercises.id"), nullable=False)
-    sets = Column(Integer, nullable=False)
-    reps = Column(Integer, nullable=False)
-    weight = Column(Float)
-
-    # relationship
-    exercise = relationship("Exercise", back_populates="training_exercises")
-    card_exercises = relationship("TrainingCardExercise", back_populates="training_exercise", cascade="all, delete-orphan")
-
-
-# TRAINING CARDS
+# TRAINING CARD
 class TrainingCard(Base):
     __tablename__ = "training_cards"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
     init_date = Column(Date, nullable=False)
     expiry_date = Column(Date, nullable=False)
+
     description = Column(String(255))
     note = Column(String(255))
 
-    # relationship
-    user = relationship("User", back_populates="training_cards")
-    exercises = relationship("TrainingCardExercise", back_populates="training_card", cascade="all, delete-orphan")
+    # relationships
+    exercises = relationship("TrainingCardExercise", back_populates="training_card",
+                             cascade="all, delete-orphan")
 
 
 # TRAINING CARD EXERCISE (position + day per user)
@@ -177,14 +187,21 @@ class TrainingCardExercise(Base):
     __tablename__ = "training_card_exercises"
 
     id = Column(Integer, primary_key=True, index=True)
-    training_card_id = Column(Integer, ForeignKey("training_cards.id"), nullable=False)
-    training_exercise_id = Column(Integer, ForeignKey("training_exercises.id"), nullable=False)
+
+    training_card_id = Column(Integer, ForeignKey("training_cards.id", ondelete="CASCADE"), nullable=False)
+
+    exercise_id = Column(Integer,ForeignKey("exercises.id", ondelete="CASCADE"),nullable=False)
+
+    day_execution = Column(Enum(WeekDay), nullable=False)
     position = Column(Integer, nullable=False)
-    day_execution = Column(String(20), nullable=False)
+
     sets = Column(Integer, nullable=False)
     reps = Column(Integer, nullable=False)
     weight = Column(Float)
 
-    # relationship
+    # relationships
     training_card = relationship("TrainingCard", back_populates="exercises")
-    training_exercise = relationship("TrainingExercise", back_populates="card_exercises")
+    exercise = relationship("Exercise", back_populates="card_exercises")
+
+    __table_args__ = (UniqueConstraint("training_card_id", "day_execution", "position",
+                                       name="uq_card_day_position"))
